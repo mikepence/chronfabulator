4c059c63f70a37ae2610588e14bc8f58174c92fd
Prelang Builder <builder@prelang.com>
2014-04-10 14:09:55 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chronfabulator::Application.config.secret_key_base = 'aaaf7f2bed1d62bdd9b5f68d46315c808dddb2af04800e35570833b37142672f33129cd0c77f3d8a1316d2bc33a12e5ccfc4c244e8d43d63e457b167ff62d11f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chronfabulator::Application.config.session_store :cookie_store, key: '_chronfabulator_session'

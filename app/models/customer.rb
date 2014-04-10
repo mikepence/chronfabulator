d448f05cd57dca63ba5b38996131eea3f0ea1df9
Prelang Builder <builder@prelang.com>
2014-04-10 14:10:53 +0000

    Defined validation for attribute 'website' on model 'Customer'

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,5 @@ class Customer < ActiveRecord::Base
   validates_formatting_of :fax_1, using: :us_phone
   validates_formatting_of :fax_2, using: :us_phone
   validates_formatting_of :email, using: :email
+  validates_formatting_of :website, using: :url
 end

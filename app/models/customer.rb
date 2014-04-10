3be26130a5d257ac278b6478c31f9828fd1a9c8a
Prelang Builder <builder@prelang.com>
2014-04-10 14:10:52 +0000

    Defined validation for attribute 'phone_2' on model 'Customer'

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < ActiveRecord::Base
   has_many :projects
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :phone_1, using: :us_phone
+  validates_formatting_of :phone_2, using: :us_phone
 end

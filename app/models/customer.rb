9cc247bab754ed2a6f3e552bdbe3ac7dc1bc982c
Prelang Builder <builder@prelang.com>
2014-04-10 14:10:52 +0000

    Defined validation for attribute 'fax_1' on model 'Customer'

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < ActiveRecord::Base
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :phone_1, using: :us_phone
   validates_formatting_of :phone_2, using: :us_phone
+  validates_formatting_of :fax_1, using: :us_phone
 end
